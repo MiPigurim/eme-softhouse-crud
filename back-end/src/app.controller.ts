1a7014d839f32376a37d47edcdbfeacb6e444570
MiPigurim <michelepigurim26@gmail.com>
2024-08-22 20:24:17 -0300

    refactor: delete app.service

--- back-end/src/app.controller.ts
+++ back-end/src/app.controller.ts
@@ -1,26 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { PrismaService } from './database/prisma.service';
-//import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(
     private prisma: PrismaService,
   ) {}
-
-  @Get()
-  async get() {
-    const produto = await this.prisma.produtos.create({
-      data: {
-        nome: 'Produto 1',
-        preco: 10.5,
-        estoque: 10,
-      },
-    });
-
-    return {
-      produto
-    }
-  }
-  }
+}
 
--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-//import { AppService } from './app.service';
 import { PrismaService } from './database/prisma.service';
 import { ProductModule } from './product/product.module';
 
