93c3b32b843c484aa2daf082a26ed85fb137e07f
MiPigurim <michelepigurim26@gmail.com>
2024-08-23 15:39:08 -0300

    feat: add exemple table

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ProductsTable from './components/products-table';
 
 function App() {
   return (
     <div className="App">
+      <ProductsTable />
 
     </div>
   );
