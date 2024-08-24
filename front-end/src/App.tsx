1a6025679b4b8623b7eed1742d9f28f2079c4ab9
MiPigurim <michelepigurim26@gmail.com>
2024-08-24 18:40:00 -0300

    refator: app typo

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ProductsTable from './components/products-table';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import { Container } from 'react-bootstrap';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ProductsTable from "./components/products-table";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <Container fluid className="App">
       <ProductsTable />
-
     </Container>
   );
 }
